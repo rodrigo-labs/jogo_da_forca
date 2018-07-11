0b78db101ae0a13c0dc715f8525ab6c59f46e11d
Rodrigo Mendes Vieira <rodrigomv.dev@gmail.com>
2018-07-11 16:30:14 -0300

    created tests for the method guess created method guess and the helper method match_regex

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,12 +1,25 @@
 # Hangman jogo da forca
+import re
+
 
 class HangMan(object):
 
     def __init__(self):
-        pass
+        self.secret_word = self.get_word()
 
     def get_word(self):
         return "palavra"
+
+    def guess(self, letter):
+        if not self.match_regex(letter):
+            raise ValueError("Caracter inválido.")
+        elif len(letter) > 1:
+            raise ValueError("Mais de um caracter")
+        letter = letter.lower()
+        return letter in self.secret_word
+
+    def match_regex(self, letter):
+        return re.match("[A-Za-záàâãéèêíïóôõöúçÁÀÂÃÉÈÍÏÓÔÕÖÚÇ]", letter)
 
 
 def main():
--- test/test_hangman.py
+++ test/test_hangman.py
@@ -10,3 +10,32 @@ class TestHangMan(TestCase):
 
     def test_get_word(self):
         self.assertEqual(self.game.get_word(), "palavra")
+
+    def test_guess_correct(self):
+        self.assertTrue(self.game.guess("a"))
+
+    def test_guess_error(self):
+        self.assertFalse(self.game.guess("z"))
+
+    def test_guess_uppercase(self):
+        self.assertTrue(self.game.guess("A"))
+
+    def test_guess_two_letter(self):
+        with self.assertRaises(Exception):
+            self.assertFalse(self.game.guess("aa"))
+
+    def test_guess_empty_string(self):
+        with self.assertRaises(Exception):
+            self.assertFalse(self.game.guess(""))
+
+    def test_guess_space(self):
+        with self.assertRaises(Exception):    
+            self.assertFalse(self.game.guess(" "))
+
+    def test_guess_number(self):
+        with self.assertRaises(Exception):
+            self.assertFalse(self.game.guess("5"))
+
+    def test_guess_another_character(self):
+        with self.assertRaises(Exception):
+            self.assertFalse(self.game.guess("+"))
